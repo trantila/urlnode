edc3647e48b2a1e03619d7c5acedec75fe8821a5
Tarmo Räntilä <tarmo.j.rantila@gmail.com>
2015-02-05 10:51:25 +0200

    A rude implementation for /shorten + view URLs in index.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,55 @@
 var express = require('express');
+var debug = require('debug')('urlnode:routes');
 var router = express.Router();
+
+//////////////////////////////////////////////////////////////////////////////
+/// LOGIC
+//////////////////////////////////////////////////////////////////////////////
+
+// Initialize an empty up-scoped variable as "DB"
+var db = {};
+
+/// Create an ID of length 5. Collisions are handled by try-try-try-again.
+function createId() {
+    var id = null;
+    var chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+    // Try five times. Repeated collisions unprobable enough for now!
+    for (var tryouts=0; tryouts<5; tryouts++) {
+        for (var i=0; i<5; i++)
+            id += chars.charAt(Math.floor(Math.random() * chars.length));
+        // If the entry is unique (and not built-in) break and be happy with it
+        if (typeof db[id] === "undefined")
+            break;
+    }
+
+    return id;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+/// ROUTES
+//////////////////////////////////////////////////////////////////////////////
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' , db: JSON.stringify(db, null, "\t")});
+});
+
+router.post("/shorten", function(req, res, next) {
+    debug(["POST -> /shorten ", JSON.stringify(req.body)].join(""));
+    // Check that request has param link (as url-encoded)
+    if (!req.body.link) {
+        res.status(400).send("Request missing parameter link.");
+    } else {
+        // Create a random ID and store the link. If the ID is falsey, generation failed.
+        id = createId();
+        if (!id) {
+            res.status(500).send("Could not generate an shorthand URL. You may try again.");
+        } else {
+            db[id] = req.body.link;
+            res.status(200).send(id);
+        } 
+    }
 });
 
 module.exports = router;
