0691c8027aae92d1998ee817297f713b5cecbc00
Tarmo Räntilä <tarmo.j.rantila@gmail.com>
2015-03-12 19:57:16 +0200

    [WIP] Button enable/disable.

--- public/scripts/index.html.js
+++ public/scripts/index.html.js
@@ -1,6 +1,30 @@
 /*
  * URLNode namespace
  */
-function urlnode() {
-    
-}
+(function urlnode (root) {
+    function yes(arg) {
+        return !!arg;
+    }
+
+    root.urlnode = {
+        "yes": yes
+    };
+})(window);
+
+
+///////////////////////////////////////////////////////////////////////////////
+// ON PAGE LOAD
+///////////////////////////////////////////////////////////////////////////////
+
+$(function () {
+    var clicks = $("button").asEventStream("click").log();
+    var text = $("input").asEventStream("keyup").map(function (e) {
+        return $(e.target).val();
+    }).toProperty().log();
+
+    var buttonEnabled = text.map(urlnode.yes).log();
+
+    buttonEnabled.onValue(function(enabled) {
+        $("button").attr("disabled", !enabled);
+    });
+});
